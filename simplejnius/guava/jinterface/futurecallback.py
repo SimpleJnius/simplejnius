07dc9e1d291a0299e7aa119f7426302c93fe0335
Kenechukwu Akubue <kengoon19@gmail.com>
2025-12-15 19:30:31 +0100

    Bump version to 0.2.0, add comprehensive bindings for Google Guava Futures APIs

--- simplejnius/guava/jinterface/futurecallback.py
+++ simplejnius/guava/jinterface/futurecallback.py
@@ -7,18 +7,14 @@ class FutureCallback(PythonJavaClass):
     __javainterfaces__ = ["com/google/common/util/concurrent/FutureCallback"]
     __javacontext__ = "app"
 
-    def __init__(self, callback: dict):
-        self.callback = callback
-
-    def _callback(self, name, *args):
-        func = self.callback.get(name)
-        if func:
-            return func(*args)
+    def __init__(self, on_success, on_failure):
+        self.on_success = on_success
+        self.on_failure = on_failure
 
     @java_method("(Ljava/lang/Object;)V")
     def onSuccess(self, obj):
-        self._callback("on_success", obj)
+        self.on_success(obj)
 
     @java_method("(Ljava/lang/Throwable;)V")
     def onFailure(self, throwable):
-        self._callback("on_failure", throwable)
+        self.on_failure(throwable)
